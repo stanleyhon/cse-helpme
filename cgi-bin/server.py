dba065e69bfd764d63040e9e1a3b216706e62e23
Stanley Hon <stanleyhon348@gmail.com>
2014-10-11 15:32:25 +1100

    Stubbed out most of the API

--- cgi-bin/server.py
+++ cgi-bin/server.py
@@ -6,7 +6,12 @@ cgitb.enable()
 
 #cgi.test()
 
-courses = ["COMP1917"]
+def statusOK ():
+    response = {"status" : "OK"}
+    print json.dumps(response)
+    exit()
+
+courses = ["COMP1917","COMP1927","COMP2121","COMP2911","COMP3331","COMP3821","COMP3891"]
 
 print "Content-Type: text/html"
 print
@@ -18,14 +23,17 @@ if "action" not in form:
     print json.dumps(response)
     exit()
 
-string = form['course'].value
-print string
-
-# SOMEONE NEEDS HELP
-if form["action"].value == "help":
-    # TODO: Validate ID
+# TODO: Validate ID
+if "id" in form:
     myid = form["id"].value
-    
+else:
+    response = {"status" : "ID Missing"}
+    print json.dumps(response)
+    exit()
+
+
+# --- HELP ---
+if form["action"].value == "help":    
     # Ensure course is legit
     if form["course"].value not in courses:
         response = {"status" : "Invalid course"}
@@ -52,9 +60,74 @@ if form["action"].value == "help":
     # DB insertion with all that stuff
     print "Your id is " + myid + ". Your course is " + course + ". Your duration is " + str(duration) + ". Your description is " + description + "."
 
-# SOMEONE POLLING TO LOOK FOR JOBS
-elif form["action"].value == "poll":
-    # TODO: Validate ID
-    print "oops"
-    
+    statusOK()
 
+# --- POLL ---
+elif form["action"].value == "poll":
+    
+    # TODO: Look up skills of this guy
+    # TODO: Look up applicable jobs for this guy
+    # TODO: Send it
+    response = {"status" : "OK", "job1" : {"id" : "shon", "course" : "COMP1917", "expiry" : "136", "Location" : "drum07", "Description" : "im bad", "status" : "in progress"}}
+    print json.dumps(response)
+
+# --- START ---
+elif form["action"].value == "start":
+    if "skills" not in form:
+        response = {"status" : "Missing skills"}
+        print json.dumps(response)
+        exit()
+
+    skills = form["skills"].value.split(',')
+    # Step through and make sure all the courses are in the master courselist
+    for skill in skills:
+        if skill not in courses:
+            response = {"status" : "Invalid skill"}
+            print json.dumps(response)
+            exit()
+    
+    # All skills are valid
+
+    # TODO: Call DB new user with myid and skills
+
+    statusOK()
+
+# --- RESPOND ---
+elif form["action"].value == "respond":
+    # TODO: Check this user already exists
+    exists = True
+    if not exists:
+        response = {"status" : "Unknown responder"}
+        print json.dumps(response)
+        exit()
+
+    # TODO: Check who they're responding to
+    if "job" not in form:
+        response = {"status" : "Missing job"}
+        print json.dumps(response)
+        exit()
+
+    job = form["job"].value
+    # TODO: Check to see if this job is in existance
+    exists = True
+    if not exists:
+        response = {"status" : "Can't find specified job"}
+        print json.dumps(response)
+        exit()
+
+    # TODO: mark job as responding
+    statusOK()
+
+# --- COMPLETE ---
+elif form["action"].value == "complete":
+    # TODO: Check the user actually has a valid job
+    exists = True
+    if exists is False:
+        response = {"status" : "No Job to complete"}
+        print json.dumps(response)
+        exit()
+
+    # TODO: Make database call to complete job. 
+
+    statusOK()
+    
